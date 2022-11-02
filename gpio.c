889c1ebd0d293807147fb1059e201fce22d3233e
fernando-lahoz <76698865+fernando-lahoz@users.noreply.github.com>
2022-11-02 18:54:40 +0100

    Arreglada escribir entradas
    No escribía los 0s

--- gpio.c
+++ gpio.c
@@ -19,7 +19,9 @@ int gpio_leer(int bit_inicial, int num_bits) {
 }
 
 void gpio_escribir(int bit_inicial, int num_bits, int valor) {
-  IOSET = (valor & ((1 << num_bits) - 1)) << bit_inicial;
+  uint32_t mask = (valor & ((1 << num_bits) - 1)) << bit_inicial;
+  IOSET = mask;  //escribe los 1s
+  IOCLR = ~mask; //escribe los 0s
 }
 
 void gpio_marcar_entrada(int bit_inicial, int num_bits) {
