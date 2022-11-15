f4f7a8889fda478a81c582bdb75cea2a090d51bc
hector toral <hectortoraltrabajos@gmail.com>
2022-11-15 16:46:12 +0100

    main

--- main.c
+++ main.c
@@ -1,22 +1,32 @@
-#include "test_tableros.h"
+#include "cola_asyn.h"
+#include "cola_msg.h"
+#include "conecta4_2022.h"
+#include "g_alarmas.h"
+#include "g_boton.h"
+#include "g_energia.h"
+#include "g_io.h"
 
-void test_timer0(void);
-void test_timer1(void);
-void test_gpio(void);
-void prototipo_planificador(void);
-
-// MAIN
 int main(void) {
-  // test_tableros();
-
-  // test_timer0();
-  // test_timer1();
-  //test_gpio();
-  prototipo_planificador();
-
-  // jugar
-  // conecta4_jugar();
-
-  while (1)
-    ;  // no hay S.O., no se retorna
+  int hay_evento, hay_msg;
+  cola_encolar_msg(INICIO, 0);
+  while (1) {
+    hay_evento = cola_hay_eventos();
+    if (hay_evento) {
+      evento_t evento = cola_desencolar_eventos();
+      g_alarma_tratar_evento(evento);
+      g_energia_tratar_evento(evento);
+      g_boton_tratar_evento(evento);
+      g_io_tratar_evento(evento);
+    }
+    hay_msg = cola_hay_msg();
+    if (hay_msg) {
+      msg_t msg = cola_desencolar_msg();
+      g_alarma_tratar_mensaje(msg);
+      g_energia_tratar_mensaje(msg);
+      g_boton_tratar_mensaje(msg);
+      g_io_tratar_mensaje(msg);
+      conecta4_tratar_mensaje(msg);
+    }
+    if (!hay_evento && !hay_msg) g_energia_idle();
+  }
 }
